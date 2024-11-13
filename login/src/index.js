79fb5f570b694a818e8da8ed1870ac4ac9513b2d
Antxnrxj <antonraj0001@gmail.com>
2024-11-13 13:20:59 +0530

    check

--- login/src/index.js
+++ login/src/index.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render("login");
+});
+
+app.get('/signup', (req, res) => {
+    res.render("signup");
+});
 
 const port = 5000;
 
